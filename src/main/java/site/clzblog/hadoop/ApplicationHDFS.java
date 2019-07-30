e9f0eeb4950c709db1435ee1c289be01deaf34c6
chengli.zou <chengli.zou@gmail.com>
2019-07-30 11:29:43 +0800

    Copy from local file api for hdfs

--- src/main/java/site/clzblog/hadoop/ApplicationHDFS.java
+++ src/main/java/site/clzblog/hadoop/ApplicationHDFS.java
@@ -11,8 +11,9 @@ public class ApplicationHDFS {
         Configuration conf = new Configuration();
         conf.set("dfs.replication", "2");
         conf.set("dfs.blocksize", "64m");
-        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop01:9000/"), conf, "root");
-        fs.copyFromLocalFile(new Path(""), new Path(""));
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.1.16:9000/"), conf, "root");
+        fs.copyFromLocalFile(new Path("C:/Users/Administrator/Downloads/jdk-8u181-linux-x64.tar.gz"), new Path("/"));
+        System.out.println("Copy from local file successfully");
         fs.close();
     }
 }
