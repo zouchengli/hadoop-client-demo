5a890700c31ddd0b0e491ebb1730d3ceebf0f9f5
chengli.zou <chengli.zou@gmail.com>
2019-07-30 15:45:20 +0800

    Handwriting word count update

--- src/main/java/site/clzblog/hadoop/wc/mapper/impl/WordCountIgnoreCaseMapper.java
+++ src/main/java/site/clzblog/hadoop/wc/mapper/impl/WordCountIgnoreCaseMapper.java
@@ -3,10 +3,11 @@ package site.clzblog.hadoop.wc.mapper.impl;
 import site.clzblog.hadoop.wc.context.Context;
 import site.clzblog.hadoop.wc.mapper.Mapper;
 
-public class WordCountMapper implements Mapper {
+public class WordCountIgnoreCaseMapper implements Mapper {
     public void map(String line, Context context) {
         String[] words = line.split(" ");
         for (String word : words) {
+            word = word.toLowerCase();
             Object value = context.get(word);
             if (null == value) context.write(word, 1);
             else {
